c2ccd995e4913ddf59afc99cdc4b483af9f19225
baisalova <bajsalovamunara@gmail.com>
2022-09-11 14:57:23 -0900

    main

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+    startswiper = new Swiper(".mySwiper", {
+    lazy: true,
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+},
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+},
+});
